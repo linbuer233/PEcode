70f05dc21b80b0d622e2c2a79fc1343a3cf9e6eb
linbuer233 <2487296367@qq.com>
2024-08-20 10:50:47 +0800

    update q800:2024-08-20

--- q800.jl
+++ q800.jl
@@ -1,44 +1,4 @@
 #quesion 800
-# using ProgressMeter
-# function IsPrime(num)
-#     num <= 1 && return false
-#     for i in 2:isqrt(num)
-#         num % i == 0 && return false
-#     end
-#     return true
-# end
-
-# function HybirdIntegers(num)
-#     result = nothing
-#     for i in 2:isqrt(num)
-#         if IsPrime(i)
-#             for j in (i+1):isqrt(num)  # 确保 i != j
-#                 if IsPrime(j) && (i^j * j^i) == num
-#                     result = (num, i, j)
-#                     break  # 找到一对就退出内层循环
-#                 end
-#             end
-#         end
-#         if result !== nothing
-#             break  # 找到结果就退出外层循环
-#         end
-#     end
-#     return result
-# end
-
-# function findHI(num)
-#     progress = Progress(num, desc="Processing: ")
-#     for i in 1:num
-#         result = HybirdIntegers(i)
-#         if result !== nothing
-#             println(result)
-#         end
-#         # 更新进度条
-#         next!(progress)
-#     end
-# end
-
-# @timev findHI(8^8)
 using Primes
 
 function C(logn)
@@ -47,22 +7,28 @@ function C(logn)
     cnt = 0
     for (i, p) in enumerate(P)
         2logP[i] * p ≤ logn || break
-        q, q0 = 1, -9
-        for _ in 1:4
-            abs(q - q0) < 2 && break
-            q0, q = q, floor(Int, (logn - log(q) * p) / logP[i])
-        end
-        j = searchsortedlast(P, max(q, 2))
-        while logP[i] * P[j] + logP[j] * p ≤ logn
-            j += 1
-        end
-        while logP[i] * P[j] + logP[j] * p > logn
+        # 上一行代码等同于
+        # if 2logP[i] * P > logn
+        #     break
+        # end
+        j = length(P)
+        mid = floor(Int, (i + j) // 2)
+        while j > i
+            if logP[i] * P[mid] + logP[mid] * p > logn
+                j = mid
+                mid = floor(Int, (i + j) // 2)
+                # println(j)
+            end
+            if logP[i] * P[j] + logP[j] * p <= logn
+                # println(P[j], "\t", P[i], "\t", P[j]^P[i] * P[i]^P[j])
+                cnt += 1
+            end
             j -= 1
         end
-        cnt += j - i
     end
     cnt
 end
 
-@show @time C(log(800) * 800)
+# @show @time C(log(800))
+# @show @time C(log(800) * 800)
 @show @time C(log(800800) * 800800)
